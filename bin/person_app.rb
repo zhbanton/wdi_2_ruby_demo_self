022ed5c5817230f41144e5952d47ab1d2974b6cb
zhbanton <zhbanton@gmail.com>
2014-06-19 10:12:41 -0400

    test

--- bin/person_app.rb
+++ bin/person_app.rb
@@ -1,14 +1,26 @@
 require_relative '../lib/person'
+require "pry"
 
 joe = Person.new('Joe', 'Smoe')
+zack = Person.new('Zack', 'Banton')
 
 # change joe's status to active
-joe.status = 'active'
+#joe.status = 'active'
 
 # change  joe's status to inactive
-joe.status = 'inactive'
+#joe.status = 'inactive'
 
 # Ok, joe is signed the contract and we make him active again.
-joe.signed_contract
+#joe.signed_contract
+
+4.times do |i|
+  Person.new("Person", "#{i}")
+end
+
+puts "All my people = #{Person.all_people}"
+
+binding.pry
+
+
 
 
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,24 @@
+
 # Define a Person class
 class Person
+
+  # Funkiest thing you will ever encounter
+  # class << self
+  #   inside here everything is a class variable or method
+  # end
+
+  # Class variable
+  @@all_people = []
+  @@person_count = 0
+
+  # Class method- NOTICE THE SELF
+  def self.all_people
+    @@all_people
+  end
+
+  def self.count
+    @@all_people.size
+  end
 
   attr_reader :first_name, :status
   attr_accessor :last_name
@@ -7,7 +26,8 @@ class Person
   def initialize(fname, lname)
     @first_name, @last_name = fname, lname
     @status = 'inactive'
-  end 
+    @@all_people << self
+  end
 
   def full_name
     "#{@first_name} #{@last_name}"
@@ -17,14 +37,14 @@ class Person
     @status = new_status
 
     # Side Effect of changing a Person's status is to say it
-    %x{ say "#{full_name} has changed their status to #{status}"}
+    %x{ say "#{full_name} has changed their status to #{status}"} #x - invokes a unix command on a mac- say command- makes the computer speak
   end
 
   def signed_contract
-    # Problem: 
+    # Problem:
     # Will not call the setter method to and notify, or say, that the status has
     # changed!
-    # @status = 'active'
+    #@status = 'active'
 
     # Problem:
     # This will create a variable, status, that is ONLY local to this method!
@@ -34,4 +54,4 @@ class Person
     # This will notify, or say, that theh Person's status has changed.
     self.status = 'active'
   end
-end       
+end
